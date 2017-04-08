70aadcdc87c710d4a111816ee9b128719195198d
wangmutian <wangmutian@wangmutiandeMacBook-Air.local>
2017-04-08 23:06:27 +0800

    logs

--- app/src/main/java/com/example/wangmutian/tulingdata/MainActivity.java
+++ app/src/main/java/com/example/wangmutian/tulingdata/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.wangmutian.tulingdata;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -13,17 +17,34 @@ public class MainActivity extends AppCompatActivity implements HttpGetDataListen
 
     private HttpData mhttpdata;
     private List<ListData> lists;
+    private Button bt1;
+    private EditText et1;
+    private TextView tv1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mhttpdata= (HttpData) new HttpData("http://www.tuling123.com/openapi/api?key=a3cc529092a54ad0bf108285509d89c3&info=%E4%BD%A0%E5%8F%AB%E4%BB%80%E4%B9%88",this).execute();
+        bt1= (Button) findViewById(R.id.bt1);
+        et1= (EditText) findViewById(R.id.et1);
+        tv1= (TextView) findViewById(R.id.tv);
+        bt1.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                String str1=et1.getText().toString();
+                //System.out.println(str1);
+                mhttpdata= (HttpData) new HttpData("http://www.tuling123.com/openapi/api?key=a3cc529092a54ad0bf108285509d89c3&info="+str1+"",MainActivity.this).execute();
+            }
+        });
+        initView();
     }
 
     private void initView(){
         lists=new ArrayList<ListData>();
+
     }
 
 
@@ -31,17 +52,18 @@ public class MainActivity extends AppCompatActivity implements HttpGetDataListen
     public void getDataUrl(String data) {
 
         //System.out.println(data);
+
         parseText(data);
 
     }
-
+    //你是谁？
     public void parseText(String data){
 
         try {
             JSONObject jb=new JSONObject(data);
             lists.add(new ListData(jb.getString("text")));
 
-
+            tv1.setText(jb.getString("text"));
 //            System.out.println("=====================");
 //            System.out.println(jb.getString("code"));
 //            System.out.println(jb.getString("text"));
